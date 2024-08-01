f4c653c555b60df0f91d9bd8f5fabca1f2af9101
Junior Romero <jumiroma007@gmail.com>
2024-08-01 06:54:31 -0500

    lists

--- dataStructures/lists.py
+++ dataStructures/lists.py
@@ -1,20 +1,33 @@
 #
 # ! A list is a kind of collection
+# ?
+
+quit()
 
 pokemons = ['pikachu','lucario','dragonite','gengar','greninja']
 
-# * range function returns a list
+# ? range function returns a list
 zzz=range(5)
 print(zzz)
 
 for pkm in pokemons:
     print(pkm)
     
-# * list are mutable we can change a element of a list using the index operator
+# ? list are mutable we can change a element of a list using the index operator
 pokemons[0]='raichu'
 print(pokemons)
 
-# * how long is a list
+# ? how long is a list
 x = [1,2,'eevee',99.0]
 print(len(x)) 
 
+# ? list()
+numlist = list()
+while True:
+    inp=input('Enter a number: ')
+    if inp=='done': break
+    value=float(inp)
+    numlist.append(value)
+    
+average=sum(numlist)/len(numlist)
+print('Average', average)
