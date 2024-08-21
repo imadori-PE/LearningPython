268cd6cfe0c425fa4a186f8343635a1741513fd6
Junior Romero Maza <jumiroma007@hotmail.com>
2024-08-21 18:15:59 -0500

    using sympy

--- AlgebraWithPython/howUseSympy.py
+++ AlgebraWithPython/howUseSympy.py
@@ -1,152 +1,104 @@
 import sympy
-from sympy import symbols
+from sympy import  Eq, symbols
 from sympy.solvers import solve
 
-x = symbols('x')
+def solveForX1():
+    x = symbols('x')
+    # Put the equation here
+    eq = 2*x**2 - 4
+    result = solve(eq,x)
+    print(f'{eq} = 0 \nResult : {result}')
+    
 
-# Put the equation here
-eq = 2*x**2 - 4
+# ? Doing more with the solution
 
-solve(eq,x)
+def solveForX2():
+    x = symbols('x')
+    # Put the equation here
+    eq = 2*x - 4
+    solution = solve(eq,x)
+    print(f'\n{eq} = 0 \nResult[0] : {solution[0]}')
+    
+# ? Solving in other ways
 
-"""Prompt for someone to enter the equation, then solve"""
+def solvingOtherWays():
+    x,y = symbols('x y')
+    # First equation set equal to zero, ready to solve
+    first = 2*x - y
+    # Sympy syntax for equation equal to zero, ready to factor
+    eq1 = Eq(first,0)
+    # Sympy solve for x
+    sol = solve(eq1,x)
+    # Show factored results
+    # print("x = ", sol[0])
+    print(f'\n{eq1} \nResult : {sol[0]}')
 
-import sympy
-from sympy import symbols
-from sympy.solvers import solve
+# ? Factoring
 
-x = symbols('x')
-eq = input('Enter equation: 0 = ')
+def factoring():
+    x,y = symbols('x y')
+    # Equation set equal to zero, ready to solve
+    # #eq = x**2-4
+    eq = x**3 - 2*x**2 - 5*x + 6
+    result=sympy.factor(eq)
+    print(f'\nFactoring {eq} \nResult : {result} ')
+    
+# ? Solve for a variable
 
-print("x = ", solve(eq,x))
+def solveForAVariable():
+    # Identify all variables
+    a,b,c,x = symbols('a b c x')
 
-"""Doing more with the solution"""
+    # Left and right sides of the equal sign
+    left = 0
+    right = a*x**2 + b*x + c
 
-import sympy
-from sympy import symbols
-from sympy.solvers import solve
+    # Variable to solve for
+    variable = x
 
-x = symbols('x')
+    # Sympy equation left = right
+    eq1 = Eq(left,right)
 
-# Put the equation here
-eq = 2*x - 4
+    # Sympy solve for that variable
+    sol = solve(eq1,variable)
+    print(f'\n{eq1}')
+    # Show factored results
+    for s in sol:
+        print(variable, " = ", s)
 
-solution = solve(eq,x)
-print("x = ", solution[0])
+# TODO ------------------------------------------------------------------------------------------------------
 
-"""Multiple answers"""
+# ? Prompt for someone to enter the equation, then solve
 
-import sympy
-from sympy import symbols
-from sympy.solvers import solve
-
-x = symbols('x')
-
-eq = input('Enter equation: 0 = ')
-
-solution = solve(eq,x)
-for s in solution:
-    print("x = ", s)
-
-"""Solving in other ways"""
-
-from sympy import *
-
-
-var('x y')
-
-# First equation set equal to zero, ready to solve
-first = 2*x - y
-
-
-# Sympy syntax for equation equal to zero, ready to factor
-eq1 = Eq(first,0)
-
-# Sympy solve for x
-sol = solve(eq1,x)
-
-# Show factored results
-print("x = ", sol[0])
-
-"""Factoring"""
-
-import sympy
-from sympy import *
-
-var('x y')
-
-# Equation set equal to zero, ready to solve
-#eq = x**2-4
-eq = x**3 - 2*x**2 - 5*x + 6
-
-sympy.factor(eq)
-
-"""Explaining how each function works"""
-
-# converts string input (including fractions) to float
-def string_frac(in_string):
-    if "/" in in_string:
-        nd = in_string.split("/")
-        n = float(nd[0])
-        d = float(nd[1])
-        ans = n/d
-        return ans
-    else:
-        ans = float(in_string)
-        return ans
+def solveInputEquation():
+    try:
+        x = symbols('x')
+        eq = input('\nEnter equation: 0 = ')
+        print(sympy.factor(eq))
+        print("x = ", solve(eq,x))
+    except:
+        print('Formula Syntax Error, try again')
+        solveInputEquation()
 
 
-# Simplest one-step addition
-def one_step_add():
-    import random
-    # Display problem
-    a = random.randint(-4,10)
-    b = random.randint(2,24)
-    print("x + ", a, " = ", b)
-    ans = float(input("x = "))
-    answer = b-a
-    # Test input
-    if ans==answer:
-        print("Correct! \n")
-    else:
-        print("Try again")
-        print("The correct answer is ", answer, "\n")
+# ? Multiple answers
 
+def solveMultipleAnswers():
+    x = symbols('x')
+    eq = input('\nEnter equation with multiple answers: 0 = ')
+    solution = solve(eq,x)
+    for s in solution:
+        print("x = ", s)
+    print(sympy.factor(eq))
 
-# One-step additon with negaive numbers
-def one_step_subtract():
-    import random
-    a = random.randint(-19,-1)
-    b = random.randint(2,24)
-    print(a, " + x = ", b)
-    ans = float(input("x = "))
-    # test
-    answer = b-a
-    if ans==answer:
-        print("Correct! \n")
-    else:
-        print("Try again")
-        print("The correct answer is ", answer, "\n")
-
-# One-step multiply
-def one_step_mult():
-    # Uses string_frac(<input string>)
-    import random
-    a = random.randint(1,11)
-    b = random.randint(2,24)
-    print(a, "x = ", b)
-    ans_in = (input("x = "))
-    answer = b/a
-    # test
-    if string_frac(ans_in)==answer:
-        print("Correct! \n")
-    else:
-        print("Try again")
-        print("The correct answer is ", answer, "\n")
-
-"""Can't do string to fraction"""
-
-print(3/4)
-
-frac = float(input("fraction = "))
-print(frac)
+# ? test
+solveForX1()
+solveForX2()
+solvingOtherWays()
+factoring()
+solveForAVariable()
+print('')
+for i  in range(1,3):
+    solveInputEquation()
+    solveMultipleAnswers()
+    
