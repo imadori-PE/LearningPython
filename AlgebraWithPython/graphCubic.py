a3e5b2980570fdec07466f8e55a97d7f55de26d4
Junior Romero <jumiroma007@hotmail.com>
2024-08-29 20:08:10 -0500

    graph cube equation an cube root

---
+++ AlgebraWithPython/graphCubic.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xmin = -10
+xmax = 10
+ymin = - 10
+ymax = 10
+points = 4*(xmax-xmin)
+x = np.linspace(xmin,xmax,points)
+
+fig, ax = plt.subplots()
+plt.axis([xmin,xmax,ymin,ymax]) # window size
+plt.plot([xmin,xmax],[0,0],'b') # blue x axis
+plt.plot([0,0],[ymin,ymax], 'b') # blue y axis
+
+# plt.plot(x, x**3)
+plt.plot(x, x**3 - 2*x**2 - 3*x + 4)
+plt.show()
